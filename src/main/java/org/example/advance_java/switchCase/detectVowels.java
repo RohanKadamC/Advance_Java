b33b507760f8b802750184ced2e2a620539a8fa0
Rohan Kadam <rohan.kadam.c@gmail.com>
2026-03-27 20:34:20 +0530

    added a detectVowels java program file

--- src/main/java/org/example/advance_java/switchCase/detectVowels.java
+++ src/main/java/org/example/advance_java/switchCase/detectVowels.java
@@ -3,6 +3,13 @@ package org.example.advance_java.switchCase;
 import java.util.Scanner;
 
 public class detectVowels {
-
-
+    static void main() {
+        System.out.print ("Enter any Alphabet: ");
+        Scanner s = new Scanner(System.in);
+        char letter = s.next().charAt(0);
+        switch (letter){
+            case 'a', 'e', 'i', 'o', 'u' -> System.out.println("This is a Vowel");
+            default -> System.out.println("This letter is a Consonant ");
+        }
+    }
 }
