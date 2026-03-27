91c21a73f8ba8a5fa881b1f2f11ae8749732703f
Rohan Kadam <rohan.kadam.c@gmail.com>
2026-03-27 20:41:52 +0530

    reduced the LOC of dayOfTheWeek java program file

--- src/main/java/org/example/advance_java/switchCase/dayOfTheWeek.java
+++ src/main/java/org/example/advance_java/switchCase/dayOfTheWeek.java
@@ -8,29 +8,14 @@ public class dayOfTheWeek {
         Scanner s = new Scanner(System.in);
         int num = s.nextInt();
         switch (num){
-            case 1:
-                System.out.println("Today is Sunday");
-                break;
-            case 2:
-                System.out.println("Today is Monday");
-                break;
-            case 3:
-                System.out.println("Today is Tuesday");
-                break;
-            case 4:
-                System.out.println("Today is Wednesday");
-                break;
-            case 5:
-                System.out.println("Today is Thursday");
-                break;
-            case 6:
-                System.out.println("Today is Friday");
-                break;
-            case 7:
-                System.out.println("Today is Saturday");
-                break;
-            default:
-                System.out.println("Invalid Number!!");
+            case 1 -> System.out.println("Today is Sunday");
+            case 2 -> System.out.println("Today is Monday");
+            case 3 -> System.out.println("Today is Tuesday");
+            case 4 -> System.out.println("Today is Wednesday");
+            case 5 ->System.out.println("Today is Thursday");
+            case 6 -> System.out.println("Today is Friday");
+            case 7 ->System.out.println("Today is Saturday");
+            default ->System.out.println("Invalid Number!!");
         }
     }
 }
